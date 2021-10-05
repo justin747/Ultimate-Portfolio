cf820d83a3e7f7036be0c7f2c1ce254d61b2931a
Justin747 <justin747@gmail.com>
2021-10-04 19:34:24 -0700

    Initial Commit

---
+++ Ultimate Portfolio/Ultimate_PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ultimate_PortfolioApp.swift
+//  Ultimate Portfolio
+//
+//  Created by Justin747 on 10/4/21.
+//
+
+import SwiftUI
+
+@main
+struct Ultimate_PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
