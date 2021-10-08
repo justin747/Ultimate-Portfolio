65295e6efaf73f029c6e72b60309a694a526eed4
Justin747 <justin747@gmail.com>
2021-10-07 18:08:06 -0700

    Fixed a few bugs and added more descriptive comments
    Fixed a few bugs and added more descriptive comments

--- Ultimate Portfolio/DataController.swift
+++ Ultimate Portfolio/DataController.swift
@@ -9,78 +9,92 @@ import CoreData
 import SwiftUI
 
 class DataController: ObservableObject {
+    /// The  CloudKit container
     let container: NSPersistentCloudKitContainer
+
+    /// Initializes a data controller in memory or in permanent storage
+    
+    
+    
+    /// Defaults to permanent storage.
+    /// inMemory decides wether  to store this data in temporary memory or not.
     
     init(inMemory: Bool = false) {
         container = NSPersistentCloudKitContainer(name: "Main")
-        
+
         if inMemory {
             container.persistentStoreDescriptions.first?.url = URL(fileURLWithPath: "/dev/null")
         }
-        
-        container.loadPersistentStores{ storeDescription, error in
+
+        container.loadPersistentStores { _, error in
             if let error = error {
                 fatalError("Fatal error loading store: \(error.localizedDescription)")
             }
         }
     }
-    
-    static var preview: DataController = {
-        let dataController = DataController(inMemory: true)
-        let viewContext = dataController.container.viewContext
-        
-        do {
-            try dataController.createSampleData()
-        } catch {
-            fatalError("Fatal error creating preivew: \(error.localizedDescription)")
-        }
-        
-        return dataController
-    }()
-    
-    func createSampleData() throws {
-        let viewContext = container.viewContext
-        
-        for i in 1...5 {
-            let project = Project(context: viewContext)
-            project.title = "Project\(i)"
-            project.items = []
-            project.creationDate = Date()
-            project.closed = Bool.random()
-            
-            for j in 1...10 {
-                let item = Item(context: viewContext)
-                item.title = "Item \(j)"
-                item.creationDate = Date()
-                item.completed = Bool.random()
-                item.project = project
-                item.priority = Int16.random(in: 1...3)
-            }
-        }
-        
-        try viewContext.save()
-    }
+
+
+    /// Saves our Core Data context iff there are changes. This silently ignores
+    /// any errors caused by saving, but this should be fine because all our attributes are optional.
     
     func save() {
         if container.viewContext.hasChanges {
             try? container.viewContext.save()
         }
     }
-    
+
     func delete(_ object: NSManagedObject) {
         container.viewContext.delete(object)
     }
+
+    /// Creates example projects and items to make manual testing easier.
+    /// - Throws: An NSError sent from calling save() on the NSManagedObjectContext.
     
+    
+    func createSampleData() throws {
+        let viewContext = container.viewContext
+
+        for projectCounter in 1...5 {
+            let project = Project(context: viewContext)
+            project.title = "Project \(projectCounter)"
+            project.items = []
+            project.creationDate = Date()
+            project.closed = Bool.random()
+
+            for itemCounter in 1...10 {
+                let item = Item(context: viewContext)
+                item.title = "Item \(projectCounter)-\(itemCounter)"
+                item.creationDate = Date()
+                item.completed = false
+                item.project = project
+                item.priority = Int16.random(in: 1...3)
+                item.completed = Bool.random()
+            }
+        }
+
+        try viewContext.save()
+    }
+
     func deleteAll() {
         let fetchRequest1: NSFetchRequest<NSFetchRequestResult> = Item.fetchRequest()
         let batchDeleteRequest1 = NSBatchDeleteRequest(fetchRequest: fetchRequest1)
         _ = try? container.viewContext.execute(batchDeleteRequest1)
-        
-        func deleteAll() {
-            let fetchRequest2: NSFetchRequest<NSFetchRequestResult> = Project.fetchRequest()
-            let batchDeleteRequest2 = NSBatchDeleteRequest(fetchRequest: fetchRequest2)
-            _ = try? container.viewContext.execute(batchDeleteRequest2)
+
+        let fetchRequest2: NSFetchRequest<NSFetchRequestResult> = Project.fetchRequest()
+        let batchDeleteRequest2 = NSBatchDeleteRequest(fetchRequest: fetchRequest2)
+        _ = try? container.viewContext.execute(batchDeleteRequest2)
     }
+
+    static var preview: DataController = {
+        let dataController = DataController(inMemory: true)
+        let viewContext = dataController.container.viewContext
+
+        do {
+            try dataController.createSampleData()
+        } catch {
+            fatalError("Fatal error creating preview: \(error.localizedDescription)")
+        }
+
+        return dataController
+    }()
 }
-
-
